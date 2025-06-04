ccf391255cec669165cd052b556655498261614a
sweetcter <daylasep0175@gmail.com>
2025-06-04 14:57:57 +0700

    chore: add valiate params and translate message authorsize from english to vietnamese

--- src/middlewares/authorizeMiddleware.ts
+++ src/middlewares/authorizeMiddleware.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from 'express';
 export const authorize = (...allowedRoles: string[]) => {
     return (req: Request, res: Response, next: NextFunction) => {
         if (req.role && !allowedRoles.includes(req.role)) {
-            return next(new UnAuthorizedError('No permission to access!'));
+            return next(new UnAuthorizedError('Bạn không có quyền truy cập!'));
         }
         next();
     };
